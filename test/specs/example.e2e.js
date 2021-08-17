06ed02f6cac4ebfcafded4bca6be2a640e5152a1
rufusbennysamuel <rufusbenny@gmail.com>
2021-08-17 20:11:19 +0530

    added package-lock.json

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,6 +1,6 @@
 const LoginPage = require('../pageobjects/login.page');
 const SecurePage = require('../pageobjects/secure.page');
-
+//Just a test spec file
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await LoginPage.open();
